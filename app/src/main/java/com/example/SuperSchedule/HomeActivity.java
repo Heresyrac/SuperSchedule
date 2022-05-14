62f03cf442fc8e7fca4c3cd668985c189af78f71
Zhenxiao Yu <1354807921@qq.com>
2022-05-15 00:25:21 +0800

    Navigation

--- app/src/main/java/com/example/SuperSchedule/HomeActivity.java
+++ app/src/main/java/com/example/SuperSchedule/HomeActivity.java
@@ -2,7 +2,9 @@ package com.example.SuperSchedule;
 
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.Observer;
@@ -15,6 +17,7 @@ import androidx.navigation.ui.NavigationUI;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.example.SuperSchedule.databinding.ActivityHomeBinding;
@@ -36,6 +39,12 @@ public class HomeActivity extends AppCompatActivity {
         binding = ActivityHomeBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
 
         binding.idTextField.setPlaceholderText("This is only used for Edit");
 
@@ -131,5 +140,13 @@ public class HomeActivity extends AppCompatActivity {
                     }
                 }}
         });
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            this.finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
